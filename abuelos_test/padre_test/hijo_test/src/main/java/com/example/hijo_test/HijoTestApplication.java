b6db0722b2ceb7ebd416235e9af490c60aff3a30
Sebas5158R <sebastianrivera@gmail.com>
2024-05-06 17:50:07 -0500

    First commit

---
+++ abuelos_test/padre_test/hijo_test/src/main/java/com/example/hijo_test/HijoTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.hijo_test;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HijoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HijoTestApplication.class, args);
+    }
+
+}
---
+++ abuelos_test/padre_test/src/main/java/com/example/padre_test/PadreTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.padre_test;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadreTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadreTestApplication.class, args);
+    }
+
+}
